1aac2f1c40326cc1170e5bfd8c3a07839fb7d426
toru68 <tomas.rutfors@minervagymnasium.se>
2026-08-30 21:08:26 +0200

    Second game

--- testspel/player.tscn.gd
+++ testspel/player.tscn.gd
@@ -2,18 +2,22 @@ extends CharacterBody2D
 
 class_name Player
 
-const MAX_SPEED = 400
-const ACC = 1500
+const  MAX_SPEED: int = 400
+const ACC: int = 1500
 
 #Godots spelloop: anropas automatiskt av spelmptorn 60 ggr/sek.
 func _physics_process(delta: float) -> void:
 	
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = velocity.move_toward(direction*MAX_SPEED, ACC*delta)
 	
 	move_and_slide()
 	
-	
-	
+	#print(get_slide_collision_count())
+	#if(Input.is_action_pressed("down")):
+	#	$PlayerShape.rotation += 0.1*delta
+
+
 
 	
