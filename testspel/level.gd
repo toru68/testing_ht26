5f628c8bc2ef11837cc8b1e0600a0931f0cfb041
toru68 <tomas.rutfors@minervagymnasium.se>
2026-09-05 12:57:32 +0200

    Third game

---
+++ testspel/level.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+const ENEMY_SCENE = preload("res://enemy.tscn")
+
+var waiting_for_reload = false
+var waiting_timer = 0.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var enemy = ENEMY_SCENE.instantiate()
+	enemy.global_position = $EnemySpawnPos1.global_position
+	add_child(enemy)
+	
+	enemy.player = $Player   #Enemys referens till spelaren sätts
+	$Player.connect("dead", _on_player_dead)
+	
+	$ReloadTimer.wait_time = 0.5
+	$ReloadTimer.one_shot = true
+	
+func _process(delta: float) -> void:
+	pass
+	"""
+	if waiting_for_reload:
+		waiting_timer += delta
+		
+		if waiting_timer >= 1.0:
+			get_tree().call_deferred("reload_current_scene")
+			
+	"""
+func _on_player_dead() -> void:
+	#print("SPELAREN DÖÖÖD")
+	#get_tree().reload_current_scene()
+	#waiting_for_reload = true
+	$ReloadTimer.start()
+	
+
+func _on_reload_timer_timeout() -> void:
+	
+	get_tree().call_deferred("reload_current_scene")
--- testspel/player.tscn.gd
+++ testspel/player.tscn.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 class_name Player
 
+#signal för att meddela ett annat script att spelaren är död
+signal dead
+
 const  MAX_SPEED: int = 400
 const ACC: int = 1500
 
@@ -10,13 +13,18 @@ func _physics_process(delta: float) -> void:
 	
 	
 	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = velocity.move_toward(direction*MAX_SPEED, ACC*delta)
 	
+	#velocity är en egenskap som tillhör Characterbody. Representerar dess rörelsevektor.
+	velocity = velocity.move_toward(direction*MAX_SPEED, ACC*delta)
+	#velocity = direction * MAX_SPEED   Ingen Acceleration
+	
+	#Inbyggd rörelsefunktion: rörelse utförs enligt velocity-vector och hantera kollisioner automatiskt.
 	move_and_slide()
 	
-	#print(get_slide_collision_count())
-	#if(Input.is_action_pressed("down")):
-	#	$PlayerShape.rotation += 0.1*delta
+func die()-> void:
+	#Funktions som anropas av enemy då spelaren är tagen.
+	hide()
+	emit_signal("dead")
 
 
 
